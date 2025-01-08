0db3f2530ff93b51712cc825c7406304088f6f83
OtavioPascoal <fjoooosa@gmail.com>
2025-01-08 10:44:24 -0300

    first commit

---
+++ src/main/java/com/tavin/dsvendas_api/DsvendasApiApplication.java
@@ -0,0 +1,13 @@
+package com.tavin.dsvendas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApiApplication.class, args);
+	}
+
+}
